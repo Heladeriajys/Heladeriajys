c1d6292d22b5d9b40eee051117e260898ec7dc26
Heladería JyS <133715162+Heladeriajys@users.noreply.github.com>
2026-09-03 16:46:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const IMAGE_BY_ID = {
   7: "foto-bombon-split.jpg",
   9: "foto-pote-360-may.jpg",
   10: "foto-pote-1400-may.jpg",
-  11: "pote3lmay.jpg",
+  11: "foto-pote-3-litros.jpg",
   12: "foto-carita-may.jpg",
   13: "foto-alfabom-may.jpg",
   14: "foto-rulo-relleno.jpg",
